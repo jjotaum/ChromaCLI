3757debda6e7ab3ca699d8c589602c987362ddd8
Jota Uribe <jjotaum@gmail.com>
2020-06-17 23:26:40 -0500

    Enhanced generated files structure formatting, now colors definitions are sorted alphabetically

--- Application/Generator.swift
+++ Application/Generator.swift
@@ -31,7 +31,17 @@ extension Generator {
         let file = File(named: name, at: folder)
         let body = folder.colorDefinitions(for: platform).joined(separator: "\n\n")
         let content = platform.fileContent(body: body)
-        try? file.write(content)
+        do {
+            try file.write(content)
+            print(
+                """
+                \(file.name) was generated successfully.
+                Can be found at \(folder.path)
+                """
+            )
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
 }
